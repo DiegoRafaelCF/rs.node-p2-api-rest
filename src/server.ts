78d651cd389f683a10b028c9f621d3fff440e31b
Diego Rafael <drafael8000@gmail.com>
2023-09-24 13:50:25 -0300

    fix: server host configuration

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import { app } from "./app";
 import { env } from "./env";
 
 try {
-	app.listen({ port: env.PORT })
-		.then(() => console.log("Server is running on port 3333"));
+	app.listen({ 
+		host: "0.0.0.0",
+		port: env.PORT 
+	})
+		.then(() => console.log("HTTP server is running"));
 } catch (err) {
 	app.log.error(err);
 }
